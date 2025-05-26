4ffb1044d860601a42995111ba3249c71d9b102a
ruziba3vich <rruzibayevich@gmail.com>
2025-05-26 22:20:01 +0500

    fix embedding_service: fix paths

--- generate_embedding_protos.sh
+++ generate_embedding_protos.sh
@@ -2,6 +2,6 @@ python -m grpc_tools.protoc \
   -I./protos \
   --python_out=./server \
   --grpc_python_out=./server \
-  ./protos/embed.proto
+  ./face_recognition_protos/embed.proto
 
 # chmod +x proto_gen.sh
