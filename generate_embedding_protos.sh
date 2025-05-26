a2ae9554e01c72b3a8a1f010ec7bdf98fd30939f
ruziba3vich <rruzibayevich@gmail.com>
2025-05-26 22:24:49 +0500

    fix embedding_service: fix script

--- generate_embedding_protos.sh
+++ generate_embedding_protos.sh
@@ -1,7 +1,7 @@
 python -m grpc_tools.protoc \
-  -I./protos \
+  -I. \
   --python_out=./server \
   --grpc_python_out=./server \
-  ./face_recognition_protos/embed.proto
+  face_recognition_protos/embed.proto
 
 # chmod +x proto_gen.sh
